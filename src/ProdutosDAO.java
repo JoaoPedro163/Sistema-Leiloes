f50149303c086beaa22ec144113a42a4fb2c06ac
João Pedro <joaopedrosilvamendes1@gmail.com>
2024-06-27 23:00:11 -0300

    Implementado a funcionalidade de salvar os produtos no banco de dados na tela cadastroVIEW

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,7 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,11 +23,23 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
         //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
+        String sql = "insert into produtos (nome, valor, status) VALUES (?,?,?)";
         
+        try{
+            prep = this.conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            System.out.println("Produto cadastrado com sucesso");
+            
+        }catch(SQLException ex){
+            System.out.println("Não foi possível cadastrar o produto");
+            System.out.println(ex.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
